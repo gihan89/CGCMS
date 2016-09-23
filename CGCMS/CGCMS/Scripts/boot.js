e3bae857859949d784b5a03635dcb8238f34808a
gihan89 <gihan89@gmail.com>
2016-09-23 10:13:43 +0530

    angular 2 changes

---
+++ CGCMS/CGCMS/Scripts/boot.js
@@ -0,0 +1,5 @@
+"use strict";
+///<reference path="./../typings/globals/core-js/index.d.ts"/>
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var app_js_1 = require('./app.js');
+platform_browser_dynamic_1.bootstrap(app_js_1.AppComponent);
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # CGCMS
 Content Management System
+
+#Steps To Setup Project
+
+1. Open Command Prompt & navigate to package.json location, then run npm install (using admin)
+2. Run command "typings install"
+
+
